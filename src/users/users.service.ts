903b5df8e1028c189bf1f8b91d6d38fd19aeab67
Malloy <li3210361050@gmail.com>
2025-10-24 20:21:06 +0800

    prisma 链接 supabase 配置 自己测试 不行就多试几遍 eslint 神经病报错就重启试试

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common'
+import { PrismaService } from '../prisma.service.js'
 
 @Injectable()
 export class UsersService {
-    getTodos(): any {
-        return ''
+    constructor(private readonly prisma: PrismaService) {}
+
+    async getUser(id: bigint) {
+        const userInfo = await this.prisma.users.findUnique({
+            where: {
+                id
+            }
+        })
+        // if (!userInfo) {
+        //     throw new Error('foo')
+        // }
+        return userInfo
     }
 }
