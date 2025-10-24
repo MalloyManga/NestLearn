a4c9f85963bb97263c73026f20e01b6bc481a038
Malloy <li3210361050@gmail.com>
2025-10-24 20:47:00 +0800

    学了一下关于 路径参数 查询参数 以及 请求体 的语法

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common'
+import { UsersService } from './users.service.js'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService) {}
+    @Get(':id')
+    getUser(@Param('id') userId: bigint) {
+        console.log(userId)
+        return userId
+    }
+
+    @Get()
+    getAll(@Query('role') role: string) {
+        console.log(role)
+        return role
+    }
+
+    @Post()
+    createUser(@Body() createUser: string) {
+        // string 仅仅用于测试
+        console.log(createUser)
+        return createUser
+    }
+    // findOne(@Param('id') id: string) {
+    //     console.log('Path Parameter id:', id) // 将会打印 '123'
+    //     // 注意：从 URL 中获取的参数默认都是字符串
+    //     return `This action returns user #${id}`
+    // }
+}
